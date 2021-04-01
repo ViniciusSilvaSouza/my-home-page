c513c4e4d5df1604c65b1a31a8c170d2b890b68a
Vinicius Soares <vinicius.souza@radixeng.com.br>
2021-04-01 02:49:30 -0300

    criando notificação email copiado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import Head from 'next/head'
 import styles from './home.module.scss';
 import useClipboard from "react-use-clipboard";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 
-export default function Home() {
+
+export default function Home({ content }) {
   const [isCopied, setCopied] = useClipboard("vinicius@vsouza.tech");
+
+  const handleClick = () => toast.success('E-mail Copiado para Clipboard!')
+
   return (
     <>
       <Head>
@@ -16,8 +22,6 @@ export default function Home() {
           <h1>Desenvolvedor <span>FullStack</span>.</h1>
           <p>Redes sociais: <br />
           </p>
-
-
           <a onClick={() => window.open("https://www.linkedin.com/in/viniciussz/", "_blank")}>
             <img className={styles.icon} src="/images/linkedin.svg" alt="Linkedin Link" />
           </a>
@@ -28,17 +32,18 @@ export default function Home() {
           <a onClick={() => window.open("https://api.whatsapp.com/send?phone=5521965655641&text=Ol%C3%A1!", "_blank")}>
             <img className={styles.icon} src="/images/whatsapp.png" alt="Whatsapp Link" />
           </a>
-          <a onClick={setCopied}>
-            Was it copied? {isCopied ? "Yes! 👍" : "Nope! 👎"}
+          <a onClick={handleClick}>
+
             <img className={styles.icon} src="/images/email.png" alt="E-mail clipboard" />
           </a>
-
+          <ToastContainer />
         </section>
-
         <img className={styles.avatar} src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
 }
+
+
 
 
